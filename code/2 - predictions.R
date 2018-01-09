e4911ce24460a9ca790393b602ef7573744e28ff
MathieuMarauri <mathieu.marauri@outlook.com>
2018-01-09 11:58:51 +0100

    prediction on raw data

--- code/2 - predictions.R
+++ code/2 - predictions.R
@@ -1,5 +1,5 @@
 
-# Predictions of number of trips by hour. 
+# Predictions of number of trips by hour with all the columns and rows. 
 
 # Packages ----------------------------------------------------------------
 
@@ -7,78 +7,123 @@ library('data.table')
 library('mlr')
 
 
-# Import data -------------------------------------------------------------
-
-trips <- readRDS('data/processed/trips.rds')
-weather <- readRDS('data/processed/weather.rds')
-
-
 # Dataset preparation -----------------------------------------------------
 
-# aggreagte trips by hour of the day
-trips <- trips[, start_time := strptime(start_time, format = '%Y-%m-%d %H')]
-trips <- trips[, .(start_date = unique(start_date), 
-                   hour_of_day = unique(hour_of_day),
-                   weekday = unique(weekday),
-                   month = unique(month),
-                   year = unique(year),
-                   count = uniqueN(trip_id)),
-               by = start_time]
+# # import data
+# trips <- readRDS('data/processed/trips.rds')
+# weather <- readRDS('data/processed/weather.rds')
+# 
+# # aggreagte trips by hour of the day
+# trips <- trips[, start_time := strptime(start_time, format = '%Y-%m-%d %H')]
+# trips <- trips[, .(start_date = unique(start_date),
+#                    hour_of_day = unique(hour_of_day),
+#                    weekday = unique(weekday),
+#                    month = unique(month),
+#                    year = unique(year),
+#                    count = uniqueN(trip_id)),
+#                by = start_time]
+# 
+# # add weather data
+# trips <- merge(x = trips,
+#                y = weather,
+#                by.x = 'start_date',
+#                by.y = 'date',
+#                all.X = TRUE)
+# 
+# # coerce columns to factor
+# factor_cols <- c('weekday', 'month', 'hour_of_day', 'year', 'events_fog',
+#                 'events_rain', 'events_snow', 'events_thunderstorm')
+# trips[, (factor_cols) := lapply(.SD, as.factor), .SDcols = factor_cols]
+# 
+# # order trips by date and add rownames
+# setorderv(trips, 'start_time')
+# trips[, c('start_time', 'start_date') := NULL]
+# 
+# saveRDS(trips, 'data/trips_model.rds')
 
-# add weather data
-trips <- merge(x = trips, 
-               y = weather,
-               by.x = 'start_date',
-               by.y = 'date',
-               all.X = TRUE)
 
-# coerce columns to factor 
-factor_cols <- c('weekday', 'month', 'hour_of_day', 'year', 'events_fog', 
-                'events_rain', 'events_snow', 'events_thunderstorm')
-trips[, (factor_cols) := lapply(.SD, as.factor), .SDcols = factor_cols]
+# Initiate modelisation ---------------------------------------------------
 
-# order trips by date and add rownames 
-setorderv(trips, 'start_time')
-rownames(trips) <- trips$start_time
-trips[, c('start_time', 'start_date') := NULL]
+# load data
+trips <- readRDS('data/trips_model.rds')
+
+# create the task: regression task with target column 'count'
+task <- makeRegrTask(id = 'trips', data = trips, target = 'count')
+
+# what are the avialable learner for the prediction task?
+list_learners <- listLearners(obj = task)
+
+# what are the availbale performance measures?
+listMeasures(obj = task)
+
+# training and test set
+set.seed(123456)
+train_set <- sample(nrow(trips), (3 * nrow(trips)) / 4)
+test_set <- setdiff(1:nrow(trips), train_set)
 
 
 # Conditional tree --------------------------------------------------------
 
-# The task: create a regression task as we want to predict the numer of trips
-prediction_task <- makeRegrTask(id = 'trips', data = trips, target = 'count')
+# create the learner
+learner <- makeLearner("regr.ctree", id = 'ctree')
 
-# The learner
-# what are the avaiable learner for the prediction task?
-listLearners(obj = prediction_task)
+# train the learner
+model <- train(learner = learner, task = task, subset = train_set)
 
-# ctree
-learner <- makeLearner("regr.ctree")
+# make prediction
+predictions <- predict(object = model, task = task, subset = test_set)
 
-# Training the learner
-set.seed(123456)
-train_set <- sample(nrow(trips), (3 * nrow(trips)) / 4)
-ctree_model <- train(learner = learner, task = prediction_task, subset = train_set)
+# performacne of the model
+performance(predictions, measures = mse)
+# 247.2984
 
-# Predictions
-test_set <- setdiff(1:nrow(trips), train_set)
-ctree_pred <- predict(object = ctree_model, task = prediction_task, subset = test_set)
 
-# The performance
-# what are the availbale performance measures?
-listMeasures(obj = prediction_task)
+# Decison tree ------------------------------------------------------------
 
-performance(ctree_pred, measures = mse)
-# 269.9281
+# create the learner
+learner <- makeLearner("regr.rpart", id = 'rpart')
+
+# train the learner
+model <- train(learner = learner, task = task, subset = train_set)
+
+# make prediction
+predictions <- predict(object = model, task = task, subset = test_set)
+
+# performacne of the model
+performance(predictions, measures = mse)
+# 410.919
 
 
 # Random forest -----------------------------------------------------------
 
-prediction_task <- makeRegrTask(id = 'trips', data = trips, target = 'count')
+# create the learner
 learner <- makeLearner('regr.cforest', 
+                       id = 'rf',
                        par.vals = list(ntree = 100))
-model <- train(learner = learner, task = prediction_task, subset = train_set)
-test_set <- setdiff(1:nrow(trips), train_set)
-pred <- predict(object = model, task = prediction_task, subset = test_set)
-performance(pred, measures = mse)
+
+# train the learner
+model <- train(learner = learner, task = task, subset = train_set)
+
+# make prediction
+predictions <- predict(object = model, task = task, subset = test_set)
+
+# performacne of the model
+performance(predictions, measures = mse)
 # 188.7767
+
+
+# Gradient boosting -------------------------------------------------------
+
+# create the learner
+learner <- makeLearner('regr.blackboost', 
+                       id = 'gb')
+
+# train the learner
+model <- train(learner = learner, task = task, subset = train_set)
+
+# make prediction
+predictions <- predict(object = model, task = task, subset = test_set)
+
+# performacne of the model
+performance(predictions, measures = mse)
+# 325.0193
